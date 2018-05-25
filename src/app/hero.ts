07a66970bb8bf66260a8c6e40c7a40fab24f8c65
Taeseong Park <taeseongdev@gmail.com>
2018-05-25 15:27:58 +0900

    modify dataset

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -14,17 +14,30 @@ export interface User {
   address?: {};
   email: string;
   gender: number;
-  
+  history: History[];  
 
+}
+
+export interface History{
+  datetime: string;
+  winner: string;
 }
 
 export interface Food{
   name: string;
+  store: string;
+  register: string;
   price: number;
   category: string[];
   address: {};
   portion: number;
   grade: number;
-
+  victory: Victory[];
 }
+
+export interface Victory{
+  user: string;
+  datetime: string;
+}
+
 
